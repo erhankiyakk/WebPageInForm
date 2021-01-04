8bfa2eed3c5e471bd25e9b5f35729b6a4fcf5c7d
Erhan KİYAK <erhankiyak1389@hotmail.com>
2021-01-04 03:57:35 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(711, 350);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 350);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.Example;
+using CefSharp.Example.Handlers;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ChromiumWebBrowser chrome;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TarayiciAc();
+        }
+        private void TarayiciAc ()
+        {
+            this.WindowState = FormWindowState.Maximized;
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+            //CefSettings settings = new CefSettings();
+            var settings = new CefSettings()
+            {
+                BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                Environment.Is64BitProcess ? "x64" : "x86",
+                "CefSharp.BrowserSubprocess.exe")
+            };
+            Cef.Initialize(settings);
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            Cef.Initialize(settings);
+            chrome = new ChromiumWebBrowser("https://nlbui.edmbilisim.com.tr/EFaturaUI/");
+            this.panel1.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+            chrome.FrameLoadEnd += (sender, args) =>
+            {
+                if (args.Frame.IsMain)
+                {
+                    chrome.ExecuteScriptAsync("$('input[name=loginUserName]').val(\"kullaniciadi\")");
+                    chrome.ExecuteScriptAsync("$('input[name=loginPassword]').val(\"sifre\")");
+                    chrome.ExecuteScriptAsync("document.getElementsByClassName('btn btn-default btn-primary pull-right')[0].click()");
+                }
+            };
+            chrome.DownloadHandler = new DownloadHandler();
+        }
+
+    }
+    private Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+    {
+        if (args.Name.StartsWith("CefSharp"))
+        {
+            string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+            string architectureSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                Environment.Is64BitProcess ? "x64" : "x86",
+                assemblyName);
+
+            return File.Exists(architectureSpecificPath)
+                ? Assembly.LoadFile(architectureSpecificPath)
+                : null;
+        }
+
+        return null;
+    }
+}
+
